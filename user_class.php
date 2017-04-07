4085a1c0ff5a18a6f438fa3155e19eb87f6c0ea7
Luiz <luiz.pires11@gmail.com>
2017-04-07 19:13:31 -0300

    classe usuario teste

---
+++ user_class.php
@@ -0,0 +1,61 @@
+<?php
+
+class user
+{
+
+	private $name;
+	private $registration;
+	private $password;
+	private $birth;
+	private $complains;
+	private $email;
+
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+	
+	public function getName()
+	{
+			return $this->name;
+	}
+	
+	public function setRegistration($registration);
+	{
+			$this->registration = $registration;
+	}
+	
+	public function getRegistration();
+	{
+		return $this->registration;
+	}
+	
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
+
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	public function setBirth($birth)
+	{
+		$this->birth = $birth;
+	}
+
+	public function getBirth()
+	{
+		return $this->birth;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email = $email;
+	}
+	public function getEmail()
+	{
+		return $this->email;
+	}
+?>
