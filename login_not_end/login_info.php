4ba3a1944bd99ffff818d2a904b1370b4d7901ee
Luiz <luiz.pires11@gmail.com>
2017-04-15 15:22:00 -0300

    provavel versao definitiva dos erros e busca no banco de dados

--- login_not_end/login_info.php
+++ login_not_end/login_info.php
@@ -42,7 +42,7 @@ $pass = $_POST['pass'];
 $matricula = $_POST['matricula'];
 $db_colluns = array("Userid","Curso","Nome","Matricula","Email" ,"Senha", "Nascimento","Genero");
 
-if($erros['matriculaErr'] == ' ' && $erros['passErr'] == ' ')
+if($erros['matriculaErr'] == ' ' && $erros['passErr'] != "Please insert a pass!") //&& $erros['passErr'] == ' ')
 {
 	if(validate_all_info($matricula, $pass, $db_colluns))
 	echo "sucesso!";
@@ -52,7 +52,7 @@ if($erros['matriculaErr'] == ' ' && $erros['passErr'] == ' ')
 
 }
 
-echo $matriculaErr. "\n" . " ". $passErr. "\n";///ATENÇÃO IMPRIMINDO OS ERROS PERCEBE-SE QUE ESTOU ACESSANDO O BANCO DE DADOS E CONSEGUINDO VERIFICAR QUE TIPO DE DISCREPANCIA O USUARIO COLOCOU COMO INFORMAÇÃO//Faça o teste tire o cometario antes do echo.
+echo $matriculaErr. "\n" ." ".$passErr."\n";///ATENÇÃO IMPRIMINDO OS ERROS PERCEBE-SE QUE ESTOU ACESSANDO O BANCO DE DADOS E CONSEGUINDO VERIFICAR QUE TIPO DE DISCREPANCIA O USUARIO COLOCOU COMO INFORMAÇÃO//Faça o teste tire o cometario antes do echo.
 
 //NAO ESTA NADA PRONTO PESSOAL AINDA FALTA MUITA COISA, MAS VOU COLOCAR NO GIT PRA VCS DAREM UM OLHADA.
 //coisas que tirei da versao anterior:
